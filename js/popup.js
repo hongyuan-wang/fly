aa516e6efef8f47e8cfc72cd3c8e501026e5b23c
yuanwhy <710136012@qq.com>
2015-09-04 16:03:16 +0800

    use pac_script to proxy your website

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+$(function(){
+	$("#proxy-domain").click(function(){
+		var domain = $("#curr-domain").html().trim();
+		
+	});
+	
+	$("#turn-mode").click(function(){
+		var mode = $("#turn-mode").html().trim();
+		if(mode == "on") {
+			//use proxy
+			var config = {
+				mode: "pac_script",
+				pacScript: {
+					data: "function FindProxyForURL(url, host) {\n" +
+					  "		if(host== 'twitter.com' || host == 'www.google.com' || host == 'www.google.com.sg') {\n" +
+					  "			return 'PROXY 127.0.0.1:8080';\n" +
+					  "     }\n" +
+				      "return 'DIRECT';}"
+				}
+			};
+			chrome.proxy.settings.set({value: config, scope: 'regular'});
+			$("#turn-mode").html("off");
+		} else {
+			$("#turn-mode").html("on");
+		}
+	});
+	chrome.tabs.query({active:true}, function(tabs){
+		var link = document.createElement("a");
+		link.href = tabs[0].url;
+		$("#curr-domain").html(link.hostname);
+	});
+	
+	
+});
+	
